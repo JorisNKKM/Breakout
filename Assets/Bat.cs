993b204e3e50b2cea83ce0ebf1d378af29d4fa66
pijussJ <146200869+JorisNKKM@users.noreply.github.com>
2024-03-27 17:47:33 +0200

    wokrs

--- Assets/Bat.cs
+++ Assets/Bat.cs
@@ -2,23 +2,38 @@ using UnityEngine;
 
 public class Bat : MonoBehaviour
 {
-	public float speed = 1;
-	Rigidbody2D rb;
-	public int balls = 5;
+    public float speed = 1;
+    Rigidbody2D rb;
+    public int lives = 5;
+    public int rnd;
+    public Vector3 derection;
+    public GameObject Ball;
 
-	void Start()
-	{
-		rb = GetComponent<Rigidbody2D>();
-		Ball.onBallLose.AddListener(RespawnBall);
-	}
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Ball.onBallLose.AddListener(RespawnBall);
+    }
 
-	void Update()
-	{
-		rb.velocity = Vector2.right * Input.GetAxisRaw("Horizontal") * speed;
-	}
+    void Update()
+    {
+        rb.velocity = Vector2.right * Input.GetAxisRaw("Horizontal") * speed;
+    }
 
-	void RespawnBall()
-	{
-		print("respawn!");
-	}
+    void RespawnBall()
+    {
+        lives--;
+        if (lives > 0)
+        {
+            rnd = Random.Range(0, 10);
+            if (rnd > 5)
+            {
+                Ball.transform.rotation = Quaternion.Euler(1, 0, 0); 
+            }
+            else
+            {
+                Ball.transform.rotation = Quaternion.Euler(-1, 0, 0); 
+            }
+        }
+    }
 }
